a5228bbfaf6aa017dedb0218bcaf85cc427ad198
Lloyd Cooper <cooperc913@gmail.com>
2023-07-21 13:31:21 -0600

    Primer commit

---
+++ src/lab1p2_lloydcooperr/Lab1P2_LloydCooperr.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1p2_lloydcooperr;
+
+/**
+ *
+ * @author CUSTOMER PC
+ */
+public class Lab1P2_LloydCooperr {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

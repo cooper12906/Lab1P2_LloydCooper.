29a8df584d28e9322362b7f3a685c7b7781c9731
Lloyd Cooper <cooperc913@gmail.com>
2023-07-21 14:54:44 -0600

    recursiva newton ralphson

--- src/lab1p2_lloydcooperr/Lab1P2_LloydCooperr.java
+++ src/lab1p2_lloydcooperr/Lab1P2_LloydCooperr.java
@@ -10,7 +10,7 @@ public class Lab1P2_LloydCooperr {
         int opcion = 0;
             do {
                 System.out.println("----MENU----");
-                System.out.println("1. Aproximacion de ceros");
+                System.out.println("1. Aproximacion de ceros (Newton Raphson)");
                 System.out.println("2. Serie de Taylor");
                 System.out.println("3. Salida");
                 System.out.println("Seleccione una opcion: ");
@@ -18,7 +18,31 @@ public class Lab1P2_LloydCooperr {
                 opcion = read.nextInt();
                 switch (opcion){
                     case 1:
+                        System.out.println("Ingrese el coeficiente 'a' ");
+                        int a = read.nextInt();
+                        while (a == 0){
+                            System.out.println("El coeficiente debe ser diferente de 0");
+                            a = read.nextInt();
+                        }
                         
+                        System.out.println("Ingrese el coeficiente 'b' ");
+                        int b = read.nextInt();
+                        
+                        System.out.println("Ingrese el coeficiente 'c' ");
+                        int c = read.nextInt();
+                        
+                        int desplazamiento = 200;
+                        int iteraciones = 100;
+                        
+                        int x0Izquierda = -desplazamiento;
+                        int x0Derecha = desplazamiento;
+                        
+                        double raizIzquierda = newtonRaphson(a, b, c,x0Izquierda, iteraciones);
+                        double raizDerecha = newtonRaphson(a,b,c, x0Derecha, iteraciones);
+                        
+                        System.out.println("Ejecución 1: x0 = " + x0Izquierda + ", raíz encontrada: " + raizIzquierda);
+                        System.out.println("Ejecución 2: x0 = " + x0Derecha + ", raíz encontrada: " + raizDerecha);
+                        System.out.println("iteraciones" + iteraciones);
                         break;
                     case 2:
                         
@@ -31,4 +55,22 @@ public class Lab1P2_LloydCooperr {
             } while (opcion != 3);
     }
     
+    public static double f(int x, int a, int b, int c) {
+        return a * x * x + b * x + c;
+    }
+
+    public static double fPrima(int x, int a, int b) {
+        return 2 * a * x + b;
+    }
+
+    public static int newtonRaphson(int a, int b, int c, int x, int iteraciones) {
+        if (iteraciones == 0) {
+            return x;
+        } else {
+            double fx = f(x, a, b, c);
+            double fpx = fPrima(x, a, b);
+            double newX = x - fx / fpx;
+            return newtonRaphson(a, b, c, (int) newX, iteraciones - 1);
+        }
+    }
 }
